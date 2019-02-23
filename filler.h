7da96e944da12639067d1e08999efaca366c19c3
Vitali <aksareons@gmail.com>
2019-02-23 14:00:58 +0200

    test int

--- filler.h
+++ filler.h
@@ -6,7 +6,7 @@
 /*   By: voksenui <voksenui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 13:59:49 by voksenui          #+#    #+#             */
-/*   Updated: 2019/02/23 13:45:12 by voksenui         ###   ########.fr       */
+/*   Updated: 2019/02/23 14:00:12 by voksenui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ typedef struct s_filler
 {
 	char		p_1;
 	char		p_2;
+	int			nmap[2];
+	int			save[2];
 	int			m_y;
 	int			m_x;
 	int			t_y;
